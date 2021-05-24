791c74089cafad17425af30137a0deecdd6362a6
stefan-hash <65160081+stefan-hash@users.noreply.github.com>
2021-05-24 21:26:39 +0200

    master-php1.1

---
+++ 07-superglobales/formulario.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+
+<html lang="es">
+    <head>
+        <meta charset="uth-8" />
+        <title>Formulario en php</title>
+    </head>
+    <body>
+        <h1>formulario en php</h1>
+        <form method="POST" action="recibir.php">
+            <p>
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" />
+            </p>
+            
+            <p>
+            <label for="apellidos">Apellidos</label>
+            <input type="text" name="apellidos" />
+            </p>
+            
+            <input type="submit" value="Enviar datos" />
+            
+        </form>
+        </form>
+
+    </body>
+</html>
+
+
+
---
+++ 07-superglobales/recibir.php
@@ -0,0 +1,9 @@
+<?php
+
+echo $_POST['nombre'];
+echo $_POST['apellidos'];
+
+var_dump($_POST);
+
+?>
+
